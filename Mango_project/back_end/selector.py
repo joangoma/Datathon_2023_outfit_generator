820e6557dd6ae81e1d4dfba266d020033afd000a
Pol de los Santos Subirats <132887066+PDelos@users.noreply.github.com>
2023-11-11 20:25:12 +0100

    ADD DOCSTRINGS

--- Mango_project/back_end/selector.py
+++ Mango_project/back_end/selector.py
@@ -3,55 +3,133 @@ from rembg import new_session, remove
 import pandas as pd
 import numpy as np
 from dataclasses import dataclass
-import pandas as pd
 
 Identifier = str
 Link = str
 
-
-
-
 class Selector:
+    """
+    A class for managing outfits and product data.
+
+    Attributes:
+    - _DFoutfit: DataFrame containing outfit data
+    - _DFproduct: DataFrame containing product data
+    """
     _DFoutfit: pd.DataFrame
     _DFproduct: pd.DataFrame
     
     def __init__(self) -> None:
+        """
+        Initializes the Selector object by loading outfit and product data from CSV files.
+        """
         self._DFoutfit = pd.read_csv('../dataset/outfit_data.csv')
         self._DFproduct = pd.read_csv('../dataset/product_data.csv')
     
     @staticmethod
     def get_pixel_matrix(image):
+        """
+        Converts an image to a pixel matrix.
+
+        Parameters:
+        - image: Image object
+
+        Returns:
+        - Pixel matrix
+        """
         pixel_data = list(image.getdata())
         width, height = image.size
         pixel_matrix = [pixel_data[i * width:(i + 1) * width] for i in range(height)]
         return pixel_matrix
 
     def get_all_outfits(self) -> list[str]:
-        return self.DF_outfits()['cod_outfit'].unique().to_list()
+        """
+        Gets a list of all unique outfit identifiers.
+
+        Returns:
+        - List of outfit identifiers
+        """
+        return self.DF_outfits()['cod_outfit'].unique()
 
     def outfits_with(self, prod_id: list[Identifier]) -> list[str]:
-        return self.DF_outfits() [self.DF_outfits() ['cod_modelo_color'] == prod_id]['cod_outfit'].to_list()
+        """
+        Finds outfits that contain specific product identifiers.
+
+        Parameters:
+        - prod_id: List of product identifiers
+
+        Returns:
+        - List of outfit identifiers
+        """
+        return self.DF_outfits()[self.DF_outfits()['cod_modelo_color'].isin(prod_id)]['cod_outfit'].to_list()
     
     def outfits_check(self, prod_id: list[Identifier]) -> bool:
-        return self.DF_outfits() [self.DF_outfits() ['cod_modelo_color'] == prod_id]['cod_outfit'].nunique() == 1
+        """
+        Checks if a set of product identifiers uniquely belongs to an outfit.
+
+        Parameters:
+        - prod_id: List of product identifiers
+
+        Returns:
+        - True if the set of product identifiers uniquely belongs to an outfit, False otherwise
+        """
+        return self.DF_outfits()[self.DF_outfits()['cod_modelo_color'].isin(prod_id)]['cod_outfit'].nunique() == 1
     
     def get_outfit(self, outf_id: Identifier) -> list[Identifier]:
-        return self.DF_outfits() [self.DF_outfits() ['cod_outfit'] == outf_id]['cod_modelo_color'].to_list()
+        """
+        Gets a list of product identifiers that belong to a specific outfit.
+
+        Parameters:
+        - outf_id: Outfit identifier
+
+        Returns:
+        - List of product identifiers
+        """
+        return self.DF_outfits()[self.DF_outfits()['cod_outfit'] == outf_id]['cod_modelo_color'].to_list()
     
-    def get_product_info(self, prod_id: Identifier) -> dict:
-        return self.DF_products().loc[prod_id].to_dict()
+    def get_product_info(self, prod_id: Identifier) -> dict[str, str]:
+        """
+        Gets information about a specific product.
+
+        Parameters:
+        - prod_id: Product identifier
+
+        Returns:
+        - Dictionary containing product information
+        """
+        return self.DF_products().loc[self.DF_products()['cod_modelo_color'] == prod_id].iloc[0].to_dict()
     
-    def get_product_image(self, prod_id: Identifier) -> dict:
-        filename: str = self.get_product_info(prod_id)['des_filename']
-        return "../dataset/images/"+filename.split("/")[-1]
+    def get_product_image(self, prod_id: Identifier) -> str:
+        """
+        Gets the file path of the image associated with a specific product.
+
+        Parameters:
+        - prod_id: Product identifier
+
+        Returns:
+        - File path of the product image
+        """
+        filename = self.get_product_info(prod_id)['des_filename']
+        return "../dataset/images/" + filename.split("/")[-1]
     
     def show_product(self, prod_id: Identifier) -> None:
+        """
+        Displays the image of a specific product.
+
+        Parameters:
+        - prod_id: Product identifier
+        """
         jpg_path: str = self.get_product_info(prod_id)['des_filename']
         img = Image.open(jpg_path)
         img.show()
         return
     
     def show_outfit(self, outf_id: Identifier) -> None:
+        """
+        Displays the images of all products in a specific outfit as a collage.
+
+        Parameters:
+        - outf_id: Outfit identifier
+        """
         products: list[Identifier] = self.get_outfit(outf_id)
         jpg_paths: list[str] = [self.get_product_image(prod_id) for prod_id in products]
         images: list = [Image.open(path) for path in jpg_paths]
@@ -70,9 +148,18 @@ class Selector:
             current_width += img.width
 
         # Save the collage
-        collage.save("../dataset/outfits/"+outf_id)
+        collage.save("../dataset/outfits/" + str(outf_id) + ".png")
 
     def get_rgb(self, id):
+        """
+        Calculates the average RGB values of an image.
+
+        Parameters:
+        - id: Product identifier
+
+        Returns:
+        - Average RGB values as a NumPy array
+        """
         input_path = self.get_product_image(id)
         n = len(input_path)
         no_back_path = input_path[:n-4] + "_nback.png"
@@ -85,7 +172,7 @@ class Selector:
             output.save(no_back_path)
             img = output
         
-        pixel_matrix = get_pixel_matrix(img)
+        pixel_matrix = self.get_pixel_matrix(img)
         width,height = img.size
         suma = np.array([0,0,0,0], dtype=float)
         print(pixel_matrix[0][0])
@@ -102,11 +189,24 @@ class Selector:
     # GETTERS
     
     def DF_outfits(self) -> pd.DataFrame:
+        """
+        Gets the DataFrame containing outfit data.
+
+        Returns:
+        - DataFrame with outfit data
+        """
         return self._DFoutfit
         
     def DF_products(self) -> pd.DataFrame:
+        """
+        Gets the DataFrame containing product data.
+
+        Returns:
+        - DataFrame with product data
+        """
         return self._DFproduct
 
 
 selector = Selector()
-print(selector.get_all_outfits())
+for outfit_id in selector.get_all_outfits():
+    selector.show_outfit(outfit_id)
